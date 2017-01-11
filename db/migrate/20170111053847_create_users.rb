06f4a8f82b50cf1f101d339f59f3caa9065e6fbe
godspeedyoo <godspeedyoo@gmail.com>
2017-01-10 22:27:56 -0800

    Initial commit with User and Task model

---
+++ db/migrate/20170111053847_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.string :title
+      t.string :company
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
